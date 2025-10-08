9d109db603bd6e0fd0556f3376386e92d149fce1
Gui-dev <adrianon10@gmail.com>
2025-10-07 21:28:08 -0300

    feat: add create account controller/route

---
+++ apps/api/src/modules/users/route/auth/create-account.ts
@@ -0,0 +1,30 @@
+import type { FastifyInstance } from 'fastify'
+import type { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+import { makeCreateUser } from '../../factories/make-create-user'
+
+export const createAccount = async (app: FastifyInstance) => {
+  app.withTypeProvider<ZodTypeProvider>().post(
+    '/users',
+    {
+      schema: {
+        body: z.object({
+          name: z.string(),
+          email: z.string().email(),
+          password: z.string().min(6),
+        }),
+        201: z.object({
+          userId: z.string().uuid(),
+        }),
+      },
+    },
+    async (request, reply) => {
+      const { name, email, password } = request.body
+      const { id } = await makeCreateUser({ name, email, password })
+
+      return reply.status(201).send({
+        userId: id,
+      })
+    }
+  )
+}
