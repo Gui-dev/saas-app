89168a7a8042d87e6451285be8fd4fc10be44277
Gui-dev <adrianon10@gmail.com>
2025-10-08 17:33:30 -0300

    feat: add helper method findById

--- apps/api/src/modules/users/contracts/user-repository-contract.ts
+++ apps/api/src/modules/users/contracts/user-repository-contract.ts
@@ -1,4 +1,4 @@
-import type { Organization, User } from '@/generated/prisma'
+import type { Organization, Prisma, User } from '@/generated/prisma'
 
 export interface ICreateUser {
   name: string
@@ -7,7 +7,12 @@ export interface ICreateUser {
   organization: Organization | null
 }
 
+export type FindByIdResponse = Prisma.UserGetPayload<{
+  select: { id: true; name: true; email: true; avatarUrl: true }
+}>
+
 export interface IUserRepositoryContract {
+  findById(id: string): Promise<FindByIdResponse | null>
   findByEmail(email: string): Promise<User | null>
   create(data: ICreateUser): Promise<User>
 }
--- apps/api/src/modules/users/repositories/user-repository.ts
+++ apps/api/src/modules/users/repositories/user-repository.ts
@@ -1,11 +1,27 @@
 import type { User } from '@/generated/prisma'
 import { prisma } from '@/lib/prisma'
 import type {
+  FindByIdResponse,
   ICreateUser,
   IUserRepositoryContract,
 } from '../contracts/user-repository-contract'
 
 export class UserRepository implements IUserRepositoryContract {
+  public async findById(id: string): Promise<FindByIdResponse | null> {
+    const user = await prisma.user.findUnique({
+      where: {
+        id,
+      },
+      select: {
+        id: true,
+        name: true,
+        email: true,
+        avatarUrl: true,
+      },
+    })
+
+    return user
+  }
   public async findByEmail(email: string): Promise<User | null> {
     const user = await prisma.user.findUnique({
       where: {
