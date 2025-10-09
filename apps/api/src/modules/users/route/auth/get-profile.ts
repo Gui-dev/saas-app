e23a8178d2ad9f662ead7a4c684c5cc16d29df93
Gui-dev <adrianon10@gmail.com>
2025-10-09 04:15:23 -0300

    feat: add auth middleware

--- apps/api/src/modules/users/route/auth/get-profile.ts
+++ apps/api/src/modules/users/route/auth/get-profile.ts
@@ -2,31 +2,35 @@ import type { FastifyInstance } from 'fastify'
 import type { ZodTypeProvider } from 'fastify-type-provider-zod'
 import { z } from 'zod'
 import { makeGetProfile } from '../../factories/make-get-profile'
+import { auth } from '@/http/middlewares/auth'
 
 export const getProfile = (app: FastifyInstance) => {
-  app.withTypeProvider<ZodTypeProvider>().get(
-    '/profile',
-    {
-      schema: {
-        summary: 'Get user profile',
-        tags: ['Users'],
-        response: {
-          200: z.object({
-            user: z.object({
-              id: z.string().uuid(),
-              name: z.string().nullable(),
-              email: z.string().email(),
-              avatarUrl: z.string().url().nullable(),
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(auth)
+    .get(
+      '/profile',
+      {
+        schema: {
+          summary: 'Get user profile',
+          tags: ['Users'],
+          response: {
+            200: z.object({
+              user: z.object({
+                id: z.string().uuid(),
+                name: z.string().nullable(),
+                email: z.string().email(),
+                avatarUrl: z.string().url().nullable(),
+              }),
             }),
-          }),
+          },
         },
       },
-    },
-    async (request, reply) => {
-      const { sub: userId } = await request.jwtVerify<{ sub: string }>()
-      const { user } = await makeGetProfile({ id: userId })
+      async (request, reply) => {
+        const userId = await request.getCurrentUserId()
+        const { user } = await makeGetProfile({ id: userId })
 
-      return reply.status(200).send({ user })
-    }
-  )
+        return reply.status(200).send({ user })
+      }
+    )
 }
