fc3318fd1f13d4ef3564f774665efc0af083848e
Gui-dev <adrianon10@gmail.com>
2025-10-10 05:23:22 -0300

    feat: add make reset password factory

---
+++ apps/api/src/modules/users/factories/make-reset-password.ts
@@ -0,0 +1,19 @@
+import { TokenRepository } from '@/modules/token/repositories/token-repository'
+import { UserRepository } from '../repositories/user-repository'
+import {
+  IResetPasswordUseCaseRequest,
+  ResetPasswordUseCase,
+} from '../use-cases/reset-password'
+
+export const makeResetPassword = async ({
+  code,
+  password,
+}: IResetPasswordUseCaseRequest) => {
+  const userRepository = new UserRepository()
+  const tokenRepository = new TokenRepository()
+  const resetPassword = new ResetPasswordUseCase(
+    userRepository,
+    tokenRepository
+  )
+  await resetPassword.execute({ code, password })
+}
