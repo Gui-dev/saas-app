c459bf2fc8d86368cf7de6456d287ff51833600d
Gui-dev <adrianon10@gmail.com>
2025-10-10 04:02:51 -0300

    feat: add request password revocer use case

---
+++ apps/api/src/modules/users/use-cases/request-password-recover.ts
@@ -0,0 +1,26 @@
+import { IUserRepositoryContract } from '../contracts/user-repository-contract'
+import { ITokenRepositoryContract } from '@/modules/token/contracts/token-repository-contract'
+
+export class RequestPasswordRecoverUseCase {
+  constructor(
+    private userRepository: IUserRepositoryContract,
+    private tokenRepository: ITokenRepositoryContract
+  ) {}
+
+  public async execute(email: string): Promise<{ tokenId: string } | null> {
+    const userFromEmail = await this.userRepository.findByEmail(email)
+
+    if (!userFromEmail) {
+      return null
+    }
+
+    const token = await this.tokenRepository.create({
+      type: 'PASSWORD_RECOVER',
+      userId: userFromEmail!.id,
+    })
+
+    return {
+      tokenId: token.id,
+    }
+  }
+}
