ea8d7b22425999ac53462379fe55f53fe2192b63
Gui-dev <adrianon10@gmail.com>
2025-10-16 05:38:57 -0300

    feat: add authenticate with github use case

---
+++ apps/api/src/modules/users/use-cases/authenticate-with-github.ts
@@ -0,0 +1,52 @@
+import { BadRequestError } from '@/http/_errors/bad-request-error'
+import { IUserRepositoryContract } from '../contracts/user-repository-contract'
+import { IAccountRepositoryContract } from '@/modules/accounts/contracts/account-repository-contract'
+
+export interface IAuthenticateWithGithubRequest {
+  githubId: string
+  name: string | null
+  email: string | null
+  avatar_url: string | null
+}
+
+export class AuthenticateWithGithubUseCase {
+  constructor(
+    private userRepository: IUserRepositoryContract,
+    private accountRepository: IAccountRepositoryContract
+  ) {}
+
+  public async execute({
+    githubId,
+    name,
+    email,
+    avatar_url,
+  }: IAuthenticateWithGithubRequest) {
+    if (!email) {
+      throw new BadRequestError('YOur Github account does not have an email')
+    }
+
+    let user = await this.userRepository.findByEmail(email)
+
+    if (!user) {
+      user = await this.userRepository.create({
+        name,
+        email,
+        avatarUrl: avatar_url,
+      })
+    }
+
+    let account = await this.accountRepository.findByUserId(user.id)
+
+    if (!account) {
+      account = await this.accountRepository.create({
+        userId: user.id,
+        providerAccountId: githubId,
+        provider: 'GITHUB',
+      })
+    }
+
+    return {
+      userId: user.id,
+    }
+  }
+}
