3b42562c83386731c8a22afe708c223d51ebf02f
Gui-dev <adrianon10@gmail.com>
2025-10-18 04:13:13 -0300

    feat: add create organization use case

---
+++ apps/api/src/modules/organization/use-cases/create-organization.ts
@@ -0,0 +1,46 @@
+import { BadRequestError } from '@/http/_errors/bad-request-error'
+import { IOrganizationRepositoryContract } from '../contracts/organization-repository-contract'
+import { createSlug } from '@/utils/create-slug'
+
+export interface ICreateOrganizationUseCaseRequest {
+  ownerId: string
+  name: string
+  domain: string | undefined
+  shouldAttachUsersByDomain: boolean
+}
+
+export class CreateOrganizationUseCase {
+  constructor(
+    private organizationRepository: IOrganizationRepositoryContract
+  ) {}
+
+  public async execute({
+    ownerId,
+    name,
+    domain,
+    shouldAttachUsersByDomain,
+  }: ICreateOrganizationUseCaseRequest) {
+    if (domain) {
+      const organizationByDomain =
+        await this.organizationRepository.findByDomain(domain)
+
+      if (organizationByDomain) {
+        throw new BadRequestError(
+          'Another Organization with the same domain already exists'
+        )
+      }
+    }
+
+    const organization = await this.organizationRepository.create({
+      ownerId,
+      name,
+      domain,
+      slug: createSlug(name),
+      shouldAttachUsersByDomain,
+    })
+
+    return {
+      organizationId: organization.id,
+    }
+  }
+}
