0411a53f1b6947ecb4e70cd90dfd245b75bb23e9
Gui-dev <adrianon10@gmail.com>
2025-10-18 04:11:23 -0300

    feat: add account repository contract

---
+++ apps/api/src/modules/accounts/contracts/account-repository-contract.ts
@@ -0,0 +1,12 @@
+import { Account } from '@/generated/prisma'
+
+export interface ICreateAccount {
+  userId: string
+  provider: 'GITHUB'
+  providerAccountId: string
+}
+
+export interface IAccountRepositoryContract {
+  findByUserId(userId: string): Promise<Account | null>
+  create(data: ICreateAccount): Promise<Account>
+}
