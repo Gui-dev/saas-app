3ff9e75f238f2c2fa40b69c8064bd61637a82855
Gui-dev <adrianon10@gmail.com>
2025-10-21 04:27:17 -0300

    feat: add make get organizations factory

---
+++ apps/api/src/modules/organization/factories/make-get-organizations.ts
@@ -0,0 +1,20 @@
+import { OrganizationRepository } from '../repositories/organization-repository'
+import {
+  GetOrganizationsUseCase,
+  IGetOrganizationsUseCaseRequest,
+} from '../use-cases/get-organizations'
+
+export const makeGetOrganizations = async ({
+  userId,
+}: IGetOrganizationsUseCaseRequest) => {
+  const organizationRepository = new OrganizationRepository()
+  const getOrganizationsUseCase = new GetOrganizationsUseCase(
+    organizationRepository
+  )
+
+  const { organizations } = await getOrganizationsUseCase.execute({ userId })
+
+  return {
+    organizations,
+  }
+}
