918d20b5c4d8d6f67990e76407f6f46d081865fd
Gui-dev <adrianon10@gmail.com>
2025-10-23 05:30:10 -0300

    feat: add make update organization

---
+++ apps/api/src/modules/organization/factories/make-update-organization.ts
@@ -0,0 +1,23 @@
+import { OrganizationRepository } from '../repositories/organization-repository'
+import {
+  IUpdateOrganizationUseCaseRequest,
+  UpdateOrganizationUseCase,
+} from '../use-cases/update-organization'
+
+export const makeUpdateOrganization = async ({
+  domain,
+  organizationId,
+  name,
+  shouldAttachUsersByDomain,
+}: IUpdateOrganizationUseCaseRequest) => {
+  const organizationRepository = new OrganizationRepository()
+  const updateOrganizationUseCase = new UpdateOrganizationUseCase(
+    organizationRepository
+  )
+  await updateOrganizationUseCase.execute({
+    domain,
+    organizationId,
+    name,
+    shouldAttachUsersByDomain,
+  })
+}
