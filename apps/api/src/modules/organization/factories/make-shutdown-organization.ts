9ca0d3f88c329159acea24e6b6ccf39c930a1a58
Gui-dev <adrianon10@gmail.com>
2025-10-23 05:31:53 -0300

    feat: add make shutdown organization

---
+++ apps/api/src/modules/organization/factories/make-shutdown-organization.ts
@@ -0,0 +1,15 @@
+import { OrganizationRepository } from '../repositories/organization-repository'
+import {
+  IShutdownOrganizationUseCaseRequest,
+  ShutdownOrganizationUseCase,
+} from '../use-cases/shutdown-organization'
+
+export const makeShutdownOrganization = async ({
+  organizationId,
+}: IShutdownOrganizationUseCaseRequest) => {
+  const organizationRepository = new OrganizationRepository()
+  const shutdownOrganizationUseCase = new ShutdownOrganizationUseCase(
+    organizationRepository
+  )
+  await shutdownOrganizationUseCase.execute({ organizationId })
+}
