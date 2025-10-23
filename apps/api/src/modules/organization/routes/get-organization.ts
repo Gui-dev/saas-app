a3835978b931bf1123095047f2a334718dd8ba2d
Gui-dev <adrianon10@gmail.com>
2025-10-23 05:26:51 -0300

    feat: add response schema

--- apps/api/src/modules/organization/routes/get-organization.ts
+++ apps/api/src/modules/organization/routes/get-organization.ts
@@ -1,42 +1,46 @@
+import { auth } from '@/http/middlewares/auth'
 import { FastifyInstance } from 'fastify'
 import { ZodTypeProvider } from 'fastify-type-provider-zod'
 import { z } from 'zod'
 
 export const getOrganization = async (app: FastifyInstance) => {
-  app.withTypeProvider<ZodTypeProvider>().get(
-    '/organizations/:slug',
-    {
-      schema: {
-        summary: 'Get details from organization',
-        tags: ['Organizations'],
-        security: [{ bearerAuth: [] }],
-        params: z.object({
-          slug: z.string(),
-        }),
-        response: {
-          200: z.object({
-            organization: z.object({
-              id: z.string().uuid(),
-              ownerId: z.string().uuid(),
-              name: z.string(),
-              slug: z.string(),
-              domain: z.string().nullable(),
-              shouldAttachUsersByDomain: z.boolean(),
-              avatarUrl: z.string().url().nullable(),
-              createdAt: z.date(),
-              updatedAt: z.date(),
-            }),
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(auth)
+    .get(
+      '/organizations/:slug',
+      {
+        schema: {
+          summary: 'Get details from organization',
+          tags: ['Organizations'],
+          security: [{ bearerAuth: [] }],
+          params: z.object({
+            slug: z.string(),
           }),
+          response: {
+            200: z.object({
+              organization: z.object({
+                id: z.string().uuid(),
+                ownerId: z.string().uuid(),
+                name: z.string(),
+                slug: z.string(),
+                domain: z.string().nullable(),
+                shouldAttachUsersByDomain: z.boolean(),
+                avatarUrl: z.string().url().nullable(),
+                createdAt: z.date(),
+                updatedAt: z.date(),
+              }),
+            }),
+          },
         },
       },
-    },
-    async (request, reply) => {
-      const { slug } = request.params
-      const { organization } = await request.getUserMembership(slug)
+      async (request, reply) => {
+        const { slug } = request.params
+        const { organization } = await request.getUserMembership(slug)
 
-      return reply.status(200).send({
-        organization,
-      })
-    }
-  )
+        return reply.status(200).send({
+          organization,
+        })
+      }
+    )
 }
--- apps/api/src/modules/organization/routes/get-organizations.ts
+++ apps/api/src/modules/organization/routes/get-organizations.ts
@@ -3,46 +3,50 @@ import { ZodTypeProvider } from 'fastify-type-provider-zod'
 import { z } from 'zod'
 import { makeGetOrganizations } from '../factories/make-get-organizations'
 import { rolesSchema } from '@saas/auth'
+import { auth } from '@/http/middlewares/auth'
 
 export const getOrganizations = async (app: FastifyInstance) => {
-  app.withTypeProvider<ZodTypeProvider>().get(
-    '/organizations',
-    {
-      schema: {
-        summary: 'Get all organizations',
-        tags: ['Organizations'],
-        security: [{ bearerAuth: [] }],
-        response: {
-          200: z.object({
-            organizations: z.array(
-              z.object({
-                id: z.string().uuid(),
-                name: z.string(),
-                slug: z.string(),
-                avatarUrl: z.string().url().nullable(),
-                role: rolesSchema,
-              })
-            ),
-          }),
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(auth)
+    .get(
+      '/organizations',
+      {
+        schema: {
+          summary: 'Get all organizations',
+          tags: ['Organizations'],
+          security: [{ bearerAuth: [] }],
+          response: {
+            200: z.object({
+              organizations: z.array(
+                z.object({
+                  id: z.string().uuid(),
+                  name: z.string(),
+                  slug: z.string(),
+                  avatarUrl: z.string().url().nullable(),
+                  role: rolesSchema,
+                })
+              ),
+            }),
+          },
         },
       },
-    },
-    async (request, reply) => {
-      const userId = await request.getCurrentUserId()
-      const { organizations } = await makeGetOrganizations({ userId })
+      async (request, reply) => {
+        const userId = await request.getCurrentUserId()
+        const { organizations } = await makeGetOrganizations({ userId })
 
-      const organizationsWithUserRole = organizations.map(
-        ({ members, ...org }) => {
-          return {
-            ...org,
-            role: members[0].role,
+        const organizationsWithUserRole = organizations.map(
+          ({ members, ...org }) => {
+            return {
+              ...org,
+              role: members[0].role,
+            }
           }
-        }
-      )
+        )
 
-      return reply.status(200).send({
-        organizations: organizationsWithUserRole,
-      })
-    }
-  )
+        return reply.status(200).send({
+          organizations: organizationsWithUserRole,
+        })
+      }
+    )
 }
