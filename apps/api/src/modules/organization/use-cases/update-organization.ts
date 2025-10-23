cdcbe41384f378406e457e9c684eebafe0864982
Gui-dev <adrianon10@gmail.com>
2025-10-23 05:30:31 -0300

    feat: add update organization use case

---
+++ apps/api/src/modules/organization/use-cases/update-organization.ts
@@ -0,0 +1,47 @@
+import { BadRequestError } from '@/http/_errors/bad-request-error'
+import { IOrganizationRepositoryContract } from '../contracts/organization-repository-contract'
+
+export interface IUpdateOrganizationUseCaseRequest {
+  domain: string | undefined
+  organizationId: string
+  name: string
+  shouldAttachUsersByDomain: boolean | undefined
+}
+
+export class UpdateOrganizationUseCase {
+  constructor(
+    private organizationRepository: IOrganizationRepositoryContract
+  ) {}
+
+  public async execute({
+    domain,
+    organizationId,
+    name,
+    shouldAttachUsersByDomain,
+  }: IUpdateOrganizationUseCaseRequest) {
+    if (domain) {
+      const organizationByDomain =
+        await this.organizationRepository.getOrganizationByDomainAndNotOrganizationId(
+          {
+            domain,
+            organizationId,
+          }
+        )
+
+      if (organizationByDomain) {
+        throw new BadRequestError(
+          'Another Organization with the same domain already exists'
+        )
+      }
+    }
+
+    await this.organizationRepository.update({
+      organizationId,
+      data: {
+        name,
+        domain,
+        shouldAttachUsersByDomain,
+      },
+    })
+  }
+}
