f9b7f485ecc1244c396eb148d8587b851fbf888f
Gui-dev <adrianon10@gmail.com>
2025-10-23 05:31:28 -0300

    feat: add shutdown organization use case

---
+++ apps/api/src/modules/organization/use-cases/shutdown-organization.ts
@@ -0,0 +1,17 @@
+import { IOrganizationRepositoryContract } from '../contracts/organization-repository-contract'
+
+export interface IShutdownOrganizationUseCaseRequest {
+  organizationId: string
+}
+
+export class ShutdownOrganizationUseCase {
+  constructor(
+    private organizationRepository: IOrganizationRepositoryContract
+  ) {}
+
+  public async execute({
+    organizationId,
+  }: IShutdownOrganizationUseCaseRequest): Promise<void> {
+    await this.organizationRepository.delete(organizationId)
+  }
+}
