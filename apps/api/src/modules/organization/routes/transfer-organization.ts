2f7a1d07b5b5924104b1873fd6a0684483f33bbe
Gui-dev <adrianon10@gmail.com>
2025-10-23 05:40:15 -0300

    feat: add transfer organization controller/route

---
+++ apps/api/src/modules/organization/routes/transfer-organization.ts
@@ -0,0 +1,64 @@
+import { FastifyInstance } from 'fastify'
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+
+import { getUserPermissions } from '@/utils/get-user-permissions'
+import { auth } from '@/http/middlewares/auth'
+import { organizationSchema } from '@saas/auth'
+import { UnauthorizedError } from '@/http/_errors/unauthorized-error'
+import { makeTransferOrganization } from '../factories/make-transfer-organization'
+
+export const transferOrganization = async (app: FastifyInstance) => {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(auth)
+    .patch(
+      '/organizations/:slug/owner',
+      {
+        schema: {
+          summary: 'Transfer organization ownership',
+          tags: ['Organizations'],
+          security: [{ bearerAuth: [] }],
+          params: z.object({
+            slug: z.string(),
+          }),
+          body: z.object({
+            transferToUserId: z.string().uuid(),
+          }),
+          response: {
+            204: z.null(),
+          },
+        },
+      },
+      async (request, repply) => {
+        const { slug } = request.params
+        const { transferToUserId } = request.body
+        const userId = await request.getCurrentUserId()
+        const { organization, membership } =
+          await request.getUserMembership(slug)
+
+        const authOrganization = organizationSchema.parse({
+          id: organization.id,
+          ownerId: organization.ownerId,
+        })
+
+        const { cannot } = getUserPermissions({
+          userId,
+          role: membership.role,
+        })
+
+        if (cannot('transfer_ownership', authOrganization)) {
+          throw new UnauthorizedError(
+            'Unauthorized, you are not allowed to update this organization'
+          )
+        }
+
+        await makeTransferOrganization({
+          organizationId: organization.id,
+          transferToUserId,
+        })
+
+        return repply.status(204).send()
+      }
+    )
+}
