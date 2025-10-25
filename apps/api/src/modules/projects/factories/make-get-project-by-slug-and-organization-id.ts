90fa5ec895f80124dcc60d2896f6708688cd5f85
Gui-dev <adrianon10@gmail.com>
2025-10-25 05:36:05 -0300

    feat: add make get project by slug and organization id factory

---
+++ apps/api/src/modules/projects/factories/make-get-project-by-slug-and-organization-id.ts
@@ -0,0 +1,22 @@
+import { ProjectRepository } from '../repositories/project-repository'
+import {
+  GetProjectBySlugAndOrganizationIdUseCase,
+  IGetProjectBySlugAndOrganizationIdUseCaseRequest,
+} from '../use-case/get-project-by-slug-and-organization-id'
+
+export const makeGetProjectBySlugAndOrganizationId = async ({
+  projectSlug,
+  organizationId,
+}: IGetProjectBySlugAndOrganizationIdUseCaseRequest) => {
+  const projectRepository = new ProjectRepository()
+  const getProjectBySlugAndOrganizationIdUseCase =
+    new GetProjectBySlugAndOrganizationIdUseCase(projectRepository)
+  const { project } = await getProjectBySlugAndOrganizationIdUseCase.execute({
+    projectSlug,
+    organizationId,
+  })
+
+  return {
+    project,
+  }
+}
