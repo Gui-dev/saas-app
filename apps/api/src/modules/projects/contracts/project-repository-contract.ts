ff83ba6469d3b7b42d6a20cb8df4cac07fa03c3c
Gui-dev <adrianon10@gmail.com>
2025-10-25 05:32:10 -0300

    feat: add helper method findProjectBySlugAndOrganizationId

--- apps/api/src/modules/projects/contracts/project-repository-contract.ts
+++ apps/api/src/modules/projects/contracts/project-repository-contract.ts
@@ -1,4 +1,34 @@
-import { Project } from '@/generated/prisma'
+import { Prisma, Project } from '@/generated/prisma'
+
+export interface IFindByProjectIdAndOrganizationId {
+  projectId: string
+  organizationId: string
+}
+
+export interface IFindByProjectSlugAndOrganizationId {
+  projectSlug: string
+  organizationId: string
+}
+
+export type IGetProjectBySlugAndOrganizationIdResponse =
+  Prisma.ProjectGetPayload<{
+    select: {
+      id: true
+      ownerId: true
+      organizationId: true
+      name: true
+      description: true
+      slug: true
+      avatarUrl: true
+      owner: {
+        select: {
+          id: true
+          name: true
+          avatarUrl: true
+        }
+      }
+    }
+  }>
 
 export interface ICreateProject {
   userId: string
@@ -8,6 +38,18 @@ export interface ICreateProject {
   slug: string
 }
 
+export interface IDeleteProject {
+  ownerId: string
+  projectId: string
+}
+
 export interface IProjectRepositoryContract {
+  findByProjectIdAndOrganizationId(
+    data: IFindByProjectIdAndOrganizationId
+  ): Promise<Project | null>
+  findByProjectSlugAndOrganizationId(
+    data: IFindByProjectSlugAndOrganizationId
+  ): Promise<IGetProjectBySlugAndOrganizationIdResponse | null>
   create(data: ICreateProject): Promise<Project>
+  delete(data: IDeleteProject): Promise<void>
 }
--- apps/api/src/modules/projects/repositories/project-repository.ts
+++ apps/api/src/modules/projects/repositories/project-repository.ts
@@ -1,12 +1,60 @@
 import { Project } from '@/generated/prisma'
 import {
   ICreateProject,
+  IDeleteProject,
+  IFindByProjectIdAndOrganizationId,
+  IFindByProjectSlugAndOrganizationId,
+  IGetProjectBySlugAndOrganizationIdResponse,
   IProjectRepositoryContract,
 } from '../contracts/project-repository-contract'
 import { prisma } from '@/lib/prisma'
 import { createSlug } from '@/utils/create-slug'
 
 export class ProjectRepository implements IProjectRepositoryContract {
+  public async findByProjectIdAndOrganizationId({
+    projectId,
+    organizationId,
+  }: IFindByProjectIdAndOrganizationId): Promise<Project | null> {
+    const project = await prisma.project.findUnique({
+      where: {
+        id: projectId,
+        organizationId,
+      },
+    })
+
+    return project
+  }
+
+  public async findByProjectSlugAndOrganizationId({
+    projectSlug,
+    organizationId,
+  }: IFindByProjectSlugAndOrganizationId): Promise<IGetProjectBySlugAndOrganizationIdResponse | null> {
+    const project = await prisma.project.findUnique({
+      where: {
+        slug: projectSlug,
+        organizationId,
+      },
+      select: {
+        id: true,
+        ownerId: true,
+        organizationId: true,
+        name: true,
+        description: true,
+        slug: true,
+        avatarUrl: true,
+        owner: {
+          select: {
+            id: true,
+            name: true,
+            avatarUrl: true,
+          },
+        },
+      },
+    })
+
+    return project
+  }
+
   public async create({
     userId,
     organizationId,
@@ -25,5 +73,14 @@ export class ProjectRepository implements IProjectRepositoryContract {
     })
 
     return project
+  }
+
+  public async delete({ projectId, ownerId }: IDeleteProject): Promise<void> {
+    await prisma.project.delete({
+      where: {
+        id: projectId,
+        ownerId,
+      },
+    })
   }
 }
