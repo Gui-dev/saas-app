333c38458b392db01bccad03e0276fb8919ba360
Gui-dev <adrianon10@gmail.com>
2025-10-25 05:34:06 -0300

    feat: add delete project use case

---
+++ apps/api/src/modules/projects/use-case/delete-project.ts
@@ -0,0 +1,14 @@
+import { IProjectRepositoryContract } from '../contracts/project-repository-contract'
+
+export interface IDeleteProjectUseCaseRequest {
+  projectId: string
+  ownerId: string
+}
+
+export class DeleteProjectUseCase {
+  constructor(private projectRepository: IProjectRepositoryContract) {}
+
+  public async execute({ projectId, ownerId }: IDeleteProjectUseCaseRequest) {
+    await this.projectRepository.delete({ projectId, ownerId })
+  }
+}
