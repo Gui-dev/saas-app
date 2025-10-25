47868538daf9309635db6e17ec45f557ff90cc3e
Gui-dev <adrianon10@gmail.com>
2025-10-25 05:34:38 -0300

    feat: add make delete project factory

---
+++ apps/api/src/modules/projects/factories/make-delete-project.ts
@@ -0,0 +1,14 @@
+import { ProjectRepository } from '../repositories/project-repository'
+import {
+  DeleteProjectUseCase,
+  IDeleteProjectUseCaseRequest,
+} from '../use-case/delete-project'
+
+export const makeDeleteProject = async ({
+  projectId,
+  ownerId,
+}: IDeleteProjectUseCaseRequest) => {
+  const projectRepository = new ProjectRepository()
+  const deleteProjectUseCase = new DeleteProjectUseCase(projectRepository)
+  await deleteProjectUseCase.execute({ projectId, ownerId })
+}
