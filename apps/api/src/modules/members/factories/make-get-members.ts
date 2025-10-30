76a6fb929c1fbcda6576f9c2c470691e489390fa
Gui-dev <adrianon10@gmail.com>
2025-10-30 04:14:12 -0300

    feat: add make get members factory

---
+++ apps/api/src/modules/members/factories/make-get-members.ts
@@ -0,0 +1,17 @@
+import { MemberRepository } from '../repositories/member-repository'
+import {
+  GetMembersUseCase,
+  IGetMembersUseCaseRequest,
+} from '../use-cases/get-members'
+
+export const makeGetMembers = async ({
+  organizationId,
+}: IGetMembersUseCaseRequest) => {
+  const memberRepository = new MemberRepository()
+  const getMembersUseCase = new GetMembersUseCase(memberRepository)
+  const { members } = await getMembersUseCase.execute({ organizationId })
+
+  return {
+    members,
+  }
+}
