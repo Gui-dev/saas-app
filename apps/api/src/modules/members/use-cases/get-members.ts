13a2f18900600303eedd0de3df7eeeb1b24c49de
Gui-dev <adrianon10@gmail.com>
2025-10-30 04:13:44 -0300

    feat: add get members use case

---
+++ apps/api/src/modules/members/use-cases/get-members.ts
@@ -0,0 +1,18 @@
+import { IMemberRepositoryContract } from '../contracts/member-repository-contract'
+
+export interface IGetMembersUseCaseRequest {
+  organizationId: string
+}
+
+export class GetMembersUseCase {
+  constructor(private memberRepository: IMemberRepositoryContract) {}
+
+  public async execute({ organizationId }: IGetMembersUseCaseRequest) {
+    const members =
+      await this.memberRepository.findByOrganizationId(organizationId)
+
+    return {
+      members,
+    }
+  }
+}
