f158e1587301d0b47bc3b83d8f6b4bb0094d3365
Gui-dev <adrianon10@gmail.com>
2025-10-30 04:15:12 -0300

    feat: add update by member id use case

---
+++ apps/api/src/modules/members/use-cases/update-by-member-id.ts
@@ -0,0 +1,34 @@
+import { BadRequestError } from '@/http/_errors/bad-request-error'
+import { IMemberRepositoryContract } from '../contracts/member-repository-contract'
+
+export interface IUpdateByMemberIdUseCaseRequest {
+  memberId: string
+  organizationId: string
+  role: 'ADMIN' | 'MEMBER' | 'BILLING'
+}
+
+export class UpdateByMemberIdUseCase {
+  constructor(private memberRepository: IMemberRepositoryContract) {}
+
+  public async execute({
+    memberId,
+    organizationId,
+    role,
+  }: IUpdateByMemberIdUseCaseRequest) {
+    const member = await this.memberRepository.updateByMemberId({
+      memberId,
+      organizationId,
+      data: {
+        role,
+      },
+    })
+
+    if (!member) {
+      throw new BadRequestError('Member not found')
+    }
+
+    return {
+      memberId: member.id,
+    }
+  }
+}
