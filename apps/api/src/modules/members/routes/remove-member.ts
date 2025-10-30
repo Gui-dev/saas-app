da29b04a499d3a436e7ec4f0033fcae288a94672
Gui-dev <adrianon10@gmail.com>
2025-10-30 04:16:53 -0300

    feat: add remove member controoler/route

---
+++ apps/api/src/modules/members/routes/remove-member.ts
@@ -0,0 +1,54 @@
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { FastifyInstance } from 'fastify/types/instance'
+import z from 'zod'
+
+import { auth } from '@/http/middlewares/auth'
+import { getUserPermissions } from '@/utils/get-user-permissions'
+import { UnauthorizedError } from '@/http/_errors/unauthorized-error'
+import { rolesSchema } from '@saas/auth'
+import { makeUpdateByMemberId } from '../factories/make-update-by-member-id'
+import { makeRemoveMember } from '../factories/make-remove-member'
+
+export const removeMember = async (app: FastifyInstance) => {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(auth)
+    .delete(
+      '/organizations/:slug/members/:memberId',
+      {
+        schema: {
+          summary: 'Remove a member from an organization',
+          tags: ['Members'],
+          security: [{ bearerAuth: [] }],
+          params: z.object({
+            slug: z.string(),
+            memberId: z.string().uuid(),
+          }),
+          response: {
+            204: z.null(),
+          },
+        },
+      },
+      async (request, reply) => {
+        const { slug, memberId } = request.params
+        const userId = await request.getCurrentUserId()
+        const { organization, membership } =
+          await request.getUserMembership(slug)
+
+        const { cannot } = getUserPermissions({ userId, role: membership.role })
+
+        if (cannot('delete', 'User')) {
+          throw new UnauthorizedError(
+            'You are not allowed to remove this member'
+          )
+        }
+
+        await makeRemoveMember({
+          memberId,
+          organizationId: organization.id,
+        })
+
+        return reply.status(204).send()
+      }
+    )
+}
