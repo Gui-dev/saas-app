d3f2bc1c510c8371b2793edfd80e903b7dd729dc
Gui-dev <adrianon10@gmail.com>
2025-10-30 04:16:17 -0300

    feat: add remove member use case

---
+++ apps/api/src/modules/members/use-cases/remove-member.ts
@@ -0,0 +1,30 @@
+import { BadRequestError } from '@/http/_errors/bad-request-error'
+import { IMemberRepositoryContract } from '../contracts/member-repository-contract'
+
+export interface IRemoveMemberUseCaseRequest {
+  memberId: string
+  organizationId: string
+}
+
+export class RemoveMemberUseCase {
+  constructor(private memberRepository: IMemberRepositoryContract) {}
+
+  public async execute({
+    memberId,
+    organizationId,
+  }: IRemoveMemberUseCaseRequest) {
+    const member = await this.memberRepository.findByMemberIdAndOrganizationId({
+      memberId,
+      organizationId,
+    })
+
+    if (!member) {
+      throw new BadRequestError('Member not found')
+    }
+
+    await this.memberRepository.delete({
+      memberId,
+      organizationId,
+    })
+  }
+}
