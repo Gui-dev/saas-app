a0ceeaf1c83c3523dc859c23c61de302e56d8e9d
Gui-dev <adrianon10@gmail.com>
2025-10-30 04:12:37 -0300

    feat: add helper methods findByMemberIdAndOrganizationId, updateByMemberId and updateByUserId

--- apps/api/src/modules/members/contracts/member-repository-contract.ts
+++ apps/api/src/modules/members/contracts/member-repository-contract.ts
@@ -1,19 +1,58 @@
-import { Member } from '@/generated/prisma'
+import { Member, Prisma } from '@/generated/prisma'
+
+export interface IFindByMemberIdAndOrganizationId {
+  memberId: string
+  organizationId: string
+}
 
 export interface IFindByOrganizationIdAndUserId {
   organizationId: string
   userId: string
 }
 
-export interface IUpdateMember {
+export interface IUpdateMemberByUserId {
   organizationId: string
   userId: string
   data: Partial<Omit<Member, 'id' | 'organizationId' | 'userId' | 'createdAt'>>
 }
 
+export interface IUpdateMemberByMemberId {
+  memberId: string
+  organizationId: string
+  data: Partial<Omit<Member, 'id' | 'organizationId' | 'userId' | 'createdAt'>>
+}
+
+export type IFindByOrganizationIdResponse = Prisma.MemberGetPayload<{
+  select: {
+    id: true
+    role: true
+    user: {
+      select: {
+        id: true
+        name: true
+        email: true
+        avatarUrl: true
+      }
+    }
+  }
+}>
+
+export interface IDeleteRequest {
+  memberId: string
+  organizationId: string
+}
+
 export interface IMemberRepositoryContract {
+  findByMemberIdAndOrganizationId(
+    data: IFindByMemberIdAndOrganizationId
+  ): Promise<Member | null>
   findByOrganizationIdAndUserId(
     data: IFindByOrganizationIdAndUserId
   ): Promise<Member | null>
-  update(data: IUpdateMember): Promise<Member>
+  findByOrganizationId(
+    organizationId: string
+  ): Promise<IFindByOrganizationIdResponse[]>
+  updateByUserId(data: IUpdateMemberByUserId): Promise<Member>
+  updateByMemberId(data: IUpdateMemberByMemberId): Promise<Member>
+  delete(data: IDeleteRequest): Promise<void>
 }
--- apps/api/src/modules/members/repositories/member-repository.ts
+++ apps/api/src/modules/members/repositories/member-repository.ts
@@ -1,12 +1,30 @@
 import { Member } from '@/generated/prisma'
 import {
+  IDeleteRequest,
+  IFindByMemberIdAndOrganizationId,
   IFindByOrganizationIdAndUserId,
+  IFindByOrganizationIdResponse,
   IMemberRepositoryContract,
-  IUpdateMember,
+  IUpdateMemberByMemberId,
+  IUpdateMemberByUserId,
 } from '../contracts/member-repository-contract'
 import { prisma } from '@/lib/prisma'
 
 export class MemberRepository implements IMemberRepositoryContract {
+  public async findByMemberIdAndOrganizationId({
+    memberId,
+    organizationId,
+  }: IFindByMemberIdAndOrganizationId): Promise<Member | null> {
+    const member = await prisma.member.findUnique({
+      where: {
+        id: memberId,
+        organizationId,
+      },
+    })
+
+    return member
+  }
+
   public async findByOrganizationIdAndUserId({
     organizationId,
     userId,
@@ -23,11 +41,38 @@ export class MemberRepository implements IMemberRepositoryContract {
     return member
   }
 
-  public async update({
+  public async findByOrganizationId(
+    organizationId: string
+  ): Promise<IFindByOrganizationIdResponse[]> {
+    const members = await prisma.member.findMany({
+      where: {
+        organizationId,
+      },
+      select: {
+        id: true,
+        role: true,
+        user: {
+          select: {
+            id: true,
+            name: true,
+            email: true,
+            avatarUrl: true,
+          },
+        },
+      },
+      orderBy: {
+        role: 'asc',
+      },
+    })
+
+    return members
+  }
+
+  public async updateByUserId({
     organizationId,
     userId,
     data,
-  }: IUpdateMember): Promise<Member> {
+  }: IUpdateMemberByUserId): Promise<Member> {
     const member = await prisma.member.update({
       where: {
         userId_organizationId: {
@@ -41,5 +86,35 @@ export class MemberRepository implements IMemberRepositoryContract {
     })
 
     return member
+  }
+
+  public async updateByMemberId({
+    memberId,
+    organizationId,
+    data,
+  }: IUpdateMemberByMemberId): Promise<Member> {
+    const member = await prisma.member.update({
+      where: {
+        id: memberId,
+        organizationId,
+      },
+      data: {
+        ...data,
+      },
+    })
+
+    return member
+  }
+
+  public async delete({
+    memberId,
+    organizationId,
+  }: IDeleteRequest): Promise<void> {
+    await prisma.member.delete({
+      where: {
+        id: memberId,
+        organizationId,
+      },
+    })
   }
 }
