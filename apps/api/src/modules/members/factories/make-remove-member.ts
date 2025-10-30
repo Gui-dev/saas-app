088c1a1a4dc3bda96e1375d10fae76d8114b07ea
Gui-dev <adrianon10@gmail.com>
2025-10-30 04:16:34 -0300

    feat: add make remove member factory

---
+++ apps/api/src/modules/members/factories/make-remove-member.ts
@@ -0,0 +1,17 @@
+import { MemberRepository } from '../repositories/member-repository'
+import {
+  IRemoveMemberUseCaseRequest,
+  RemoveMemberUseCase,
+} from '../use-cases/remove-member'
+
+export const makeRemoveMember = async ({
+  memberId,
+  organizationId,
+}: IRemoveMemberUseCaseRequest) => {
+  const memberRepository = new MemberRepository()
+  const removeMemberUseCase = new RemoveMemberUseCase(memberRepository)
+  await removeMemberUseCase.execute({
+    memberId,
+    organizationId,
+  })
+}
