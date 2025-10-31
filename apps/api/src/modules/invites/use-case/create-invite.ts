e2d04e2df3a073be7032f02a85caabffab8dd5e2
Gui-dev <adrianon10@gmail.com>
2025-10-31 04:24:21 -0300

    feat: add create invite use case

---
+++ apps/api/src/modules/invites/use-case/create-invite.ts
@@ -0,0 +1,55 @@
+import { BadRequestError } from '@/http/_errors/bad-request-error'
+import { IMemberRepositoryContract } from '@/modules/members/contracts/member-repository-contract'
+import { IInviteRepositoryContract } from '../contracts/invite-repository-contract'
+
+export interface ICreateInviteUseCaseRequest {
+  organizationId: string
+  authorId: string
+  email: string
+  role: 'ADMIN' | 'MEMBER' | 'BILLING'
+}
+
+export class CreateInviteUseCase {
+  constructor(
+    private inviteRepository: IInviteRepositoryContract,
+    private memberRepository: IMemberRepositoryContract
+  ) {}
+
+  public async execute({
+    organizationId,
+    authorId,
+    email,
+    role,
+  }: ICreateInviteUseCaseRequest) {
+    const inviteWithSameEmail =
+      await this.inviteRepository.findByEmailAndOrganizationId({
+        email,
+        organizationId,
+      })
+
+    if (inviteWithSameEmail) {
+      throw new BadRequestError('Another invite with same email already exists')
+    }
+
+    const memberWithSameEmail =
+      await this.memberRepository.findByOrganizationIdAndUserEmail({
+        organizationId,
+        email,
+      })
+
+    if (memberWithSameEmail) {
+      throw new BadRequestError('Member with same email already exists')
+    }
+
+    const invite = await this.inviteRepository.create({
+      organizationId,
+      authorId,
+      email,
+      role,
+    })
+
+    return {
+      inviteId: invite.id,
+    }
+  }
+}
