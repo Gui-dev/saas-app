6d331dce305b1ed638a6716fc3a6e5b16a42f1d0
Gui-dev <adrianon10@gmail.com>
2025-11-04 04:49:37 -0300

    feat: add get invites controller/routes

---
+++ apps/api/src/modules/invites/routes/get-invites.ts
@@ -0,0 +1,66 @@
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { FastifyInstance } from 'fastify/types/instance'
+import z from 'zod'
+
+import { auth } from '@/http/middlewares/auth'
+import { getUserPermissions } from '@/utils/get-user-permissions'
+import { UnauthorizedError } from '@/http/_errors/unauthorized-error'
+import { BadRequestError } from '@/http/_errors/bad-request-error'
+import { makeGetInvites } from '../factories/make-get-invites'
+
+export const getInvites = async (app: FastifyInstance) => {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(auth)
+    .get(
+      '/organizations/:slug/invites',
+      {
+        schema: {
+          summary: 'Get all invites',
+          tags: ['Invites'],
+          security: [{ bearerAuth: [] }],
+          params: z.object({
+            slug: z.string(),
+          }),
+          response: {
+            201: z.object({
+              invites: z.array(
+                z.object({
+                  id: z.string().uuid(),
+                  email: z.string().email(),
+                  role: z.string(),
+                  createdAt: z.date(),
+                  author: z
+                    .object({
+                      id: z.string().uuid(),
+                      name: z.string().nullable(),
+                    })
+                    .nullable(),
+                })
+              ),
+            }),
+          },
+        },
+      },
+      async (request, reply) => {
+        const { slug } = request.params
+        const userId = await request.getCurrentUserId()
+        const { organization, membership } =
+          await request.getUserMembership(slug)
+
+        const { cannot } = getUserPermissions({ userId, role: membership.role })
+
+        if (cannot('get', 'Invite')) {
+          throw new UnauthorizedError(
+            'You are not allowed to get organization invites'
+          )
+        }
+
+        const { invites } = await makeGetInvites({
+          organizationId: organization.id,
+        })
+
+        return reply.status(201).send({ invites })
+      }
+    )
+}
