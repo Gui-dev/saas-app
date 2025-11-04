84bef1889cfffdd448c7d43c0187e561f2cda5de
Gui-dev <adrianon10@gmail.com>
2025-11-04 04:49:20 -0300

    feat: add make get invites factory

---
+++ apps/api/src/modules/invites/factories/make-get-invites.ts
@@ -0,0 +1,17 @@
+import { InviteRepository } from '../repositories/invite-repository'
+import {
+  GetInvitesUseCase,
+  IGetInvitesUseCaseRequest,
+} from '../use-case/get-invites'
+
+export const makeGetInvites = async ({
+  organizationId,
+}: IGetInvitesUseCaseRequest) => {
+  const inviteRepository = new InviteRepository()
+  const getInvitesUseCase = new GetInvitesUseCase(inviteRepository)
+  const { invites } = await getInvitesUseCase.execute({ organizationId })
+
+  return {
+    invites,
+  }
+}
