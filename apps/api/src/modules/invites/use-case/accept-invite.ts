d235e511c73885e30c0709695a7d29406e4f9a37
Gui-dev <adrianon10@gmail.com>
2025-11-04 05:15:50 -0300

    feat: add acceptInviteUseCase

---
+++ apps/api/src/modules/invites/use-case/accept-invite.ts
@@ -0,0 +1,40 @@
+import { BadRequestError } from '@/http/_errors/bad-request-error'
+import { IInviteRepositoryContract } from '../contracts/invite-repository-contract'
+import { IUserRepositoryContract } from '@/modules/users/contracts/user-repository-contract'
+
+export interface IAcceptInviteUseCaseRequest {
+  inviteId: string
+  userId: string
+}
+
+export class AcceptInviteUseCase {
+  constructor(
+    private inviteRepository: IInviteRepositoryContract,
+    private userRepository: IUserRepositoryContract
+  ) {}
+
+  public async execute({ inviteId, userId }: IAcceptInviteUseCaseRequest) {
+    const invite = await this.inviteRepository.findByInviteId(inviteId)
+
+    if (!invite) {
+      throw new BadRequestError('Invite not found or expired')
+    }
+
+    const user = await this.userRepository.findById(userId)
+
+    if (!user) {
+      throw new BadRequestError('User not found')
+    }
+
+    if (invite.email !== user.email) {
+      throw new BadRequestError('This invite belongs to another user')
+    }
+
+    await this.inviteRepository.acceptInvite({
+      inviteId,
+      userId,
+      organizationId: invite.organization.id,
+      role: invite.role,
+    })
+  }
+}
