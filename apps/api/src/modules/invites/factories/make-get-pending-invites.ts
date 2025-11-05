9ad3dea5c354cccddf04a5b2468c4ec4ab25ced8
Gui-dev <adrianon10@gmail.com>
2025-11-05 05:14:31 -0300

    feat: add make get pending invites factory

---
+++ apps/api/src/modules/invites/factories/make-get-pending-invites.ts
@@ -0,0 +1,22 @@
+import { UserRepository } from '@/modules/users/repositories/user-repository'
+import { InviteRepository } from '../repositories/invite-repository'
+import {
+  GetPendingInvitesUseCase,
+  IGetPendingInvitesUseCaseRequest,
+} from '../use-case/get-pending-invites'
+
+export const makeGetPendingInvites = async ({
+  userId,
+}: IGetPendingInvitesUseCaseRequest) => {
+  const inviteRepository = new InviteRepository()
+  const userRepository = new UserRepository()
+  const getPendingInvitesUsecase = new GetPendingInvitesUseCase(
+    inviteRepository,
+    userRepository
+  )
+  const { invites } = await getPendingInvitesUsecase.execute({ userId })
+
+  return {
+    invites,
+  }
+}
