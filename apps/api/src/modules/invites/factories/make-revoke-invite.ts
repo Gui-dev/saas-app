ea862c9de62be30831c6812ede03c350df7dd360
Gui-dev <adrianon10@gmail.com>
2025-11-05 04:44:06 -0300

    feat: ad make revoke invite factory

---
+++ apps/api/src/modules/invites/factories/make-revoke-invite.ts
@@ -0,0 +1,13 @@
+import { InviteRepository } from '../repositories/invite-repository'
+import {
+  IRevokeInviteUseCaseRequest,
+  RevokeInviteUseCase,
+} from '../use-case/revoke-invite'
+
+export const makeRevokeInvite = async ({
+  inviteId,
+}: IRevokeInviteUseCaseRequest) => {
+  const inviteRepository = new InviteRepository()
+  const revokeInviteUsecase = new RevokeInviteUseCase(inviteRepository)
+  await revokeInviteUsecase.execute({ inviteId })
+}
