320d99514087d233b1e0b8324d1242a81189ccaa
Gui-dev <adrianon10@gmail.com>
2025-11-06 04:47:18 -0300

    feat: add make get organization billing factory

---
+++ apps/api/src/modules/billing/factories/make-get-organization-billing.ts
@@ -0,0 +1,25 @@
+import { MemberRepository } from '@/modules/members/repositories/member-repository'
+import {
+  GetOrganizationBillingUseCase,
+  IGetOrganizationBillingUseCaseRequest,
+} from '../use-cases/get-organization-billing'
+import { ProjectRepository } from '@/modules/projects/repositories/project-repository'
+
+export const makeGetOrganizationBilling = async ({
+  organizationId,
+}: IGetOrganizationBillingUseCaseRequest) => {
+  const memberRepository = new MemberRepository()
+  const projectRepository = new ProjectRepository()
+  const getOrganizationBillingUseCase = new GetOrganizationBillingUseCase(
+    memberRepository,
+    projectRepository
+  )
+
+  const { billing } = await getOrganizationBillingUseCase.execute({
+    organizationId,
+  })
+
+  return {
+    billing,
+  }
+}
