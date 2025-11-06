f278a08f43c8d1b5f7e28770000c7bf427406334
Gui-dev <adrianon10@gmail.com>
2025-11-06 04:46:54 -0300

    feat: add get organization billing use case

---
+++ apps/api/src/modules/billing/use-cases/get-organization-billing.ts
@@ -0,0 +1,38 @@
+import { IMemberRepositoryContract } from '@/modules/members/contracts/member-repository-contract'
+import { IProjectRepositoryContract } from '@/modules/projects/contracts/project-repository-contract'
+
+export interface IGetOrganizationBillingUseCaseRequest {
+  organizationId: string
+}
+
+export class GetOrganizationBillingUseCase {
+  constructor(
+    private memberRepository: IMemberRepositoryContract,
+    private projectRepository: IProjectRepositoryContract
+  ) {}
+
+  public async execute({
+    organizationId,
+  }: IGetOrganizationBillingUseCaseRequest) {
+    const [amountOfMembers, amountOfProjects] = await Promise.all([
+      this.memberRepository.count(organizationId),
+      this.projectRepository.count(organizationId),
+    ])
+
+    return {
+      billing: {
+        seats: {
+          amount: amountOfMembers,
+          unit: 10,
+          price: amountOfMembers * 10,
+        },
+        projects: {
+          amount: amountOfProjects,
+          unit: 20,
+          price: amountOfProjects * 20,
+        },
+        total: amountOfMembers * 10 + amountOfProjects * 20,
+      },
+    }
+  }
+}
