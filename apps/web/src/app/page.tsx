4c4decbaccb51952f2667bd82917ce05f73769d3
Gui-dev <adrianon10@gmail.com>
2025-11-08 04:29:10 -0300

    chore: add initial set up

---
+++ apps/web/src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@/components/ui/button'
+
+export default function Home() {
+  return (
+    <>
+      <h1 className="flex gap-2 mb-2">Hello World</h1>
+      <Button>Click</Button>
+    </>
+  )
+}
