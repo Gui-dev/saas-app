4ad1288d07e2b0c2f6ab87563a532052d345ffa9
Gui-dev <adrianon10@gmail.com>
2025-11-13 04:58:55 -0300

    feat: add sign in implementation

--- apps/web/src/app/(auth)/sign-in/actions.tsx
+++ apps/web/src/app/(auth)/sign-in/actions.tsx
@@ -1,14 +1,68 @@
 'use server'
 
-import { api } from '@/http/api-client'
+import { z } from 'zod'
+import { cookies } from 'next/headers'
+import { HTTPError } from 'ky'
+
 import { signInWithPassword } from '@/http/sign-in--with-password'
 
-export const signInWithEmailAndPassword = async (data: FormData) => {
-  const { email, password } = Object.fromEntries(data)
-  const result = await signInWithPassword({
-    email: String(email),
-    password: String(password),
-  })
+const signInSchema = z.object({
+  email: z
+    .string({ message: 'Por favor, forneça um e-mail' })
+    .email({ message: 'Por favor, forneça um e-mail valido' }),
+  password: z
+    .string({ message: 'Por favor, forneça uma senha' })
+    .min(1, { message: 'Por favor, forneça uma senha' }),
+})
 
-  return result
+export const signInWithEmailAndPassword = async (data: FormData) => {
+  const result = signInSchema.safeParse(Object.fromEntries(data))
+  const cookieStore = await cookies()
+
+  if (!result.success) {
+    const errors = result.error.flatten().fieldErrors
+
+    return {
+      success: false,
+      message: null,
+      errors,
+    }
+  }
+
+  const { email, password } = result.data
+  try {
+    const { token } = await signInWithPassword({
+      email: String(email),
+      password: String(password),
+    })
+
+    cookieStore.set('saas-token', token, {
+      path: '/',
+      maxAge: 60 * 60 * 24 * 7,
+    })
+  } catch (error) {
+    if (error instanceof HTTPError) {
+      const { message } = await error.response.json()
+
+      return {
+        success: false,
+        message,
+        errors: null,
+      }
+    }
+
+    console.error(error)
+
+    return {
+      success: false,
+      message: 'Ocorreu um erro ao fazer login, tente novamente mais tarde',
+      errors: null,
+    }
+  }
+
+  return {
+    success: true,
+    message: null,
+    errors: null,
+  }
 }
--- apps/web/src/app/(auth)/sign-in/page.tsx
+++ apps/web/src/app/(auth)/sign-in/page.tsx
@@ -1,64 +1,7 @@
-import { Button } from '@/components/ui/button'
-import { Input } from '@/components/ui/input'
-import { Label } from '@/components/ui/label'
-import { Separator } from '@/components/ui/separator'
-import Link from 'next/link'
-import Image from 'next/image'
-
-import githubIcon from '@/assets/github-icon.svg'
-import { signInWithEmailAndPassword } from './actions'
+import { SignInForm } from './sign-in-form'
 
 const SignIn = () => {
-  return (
-    <form action={signInWithEmailAndPassword} className="space-y-4">
-      <div className="space-y-1">
-        <Label htmlFor="email">Email</Label>
-        <Input
-          type="email"
-          name="email"
-          id="email"
-          placeholder="Digite seu e-mail"
-        />
-      </div>
-
-      <div className="space-y-1">
-        <Label htmlFor="password">Senha</Label>
-        <Input
-          type="password"
-          name="password"
-          id="password"
-          placeholder="Digite sua senha"
-        />
-        <Link
-          href="/forgot-password"
-          className="text-xs font-medium text-foreground hover:underline"
-        >
-          Esqueceu sua senha?
-        </Link>
-      </div>
-
-      <Button type="submit" className="w-full">
-        Entrar
-      </Button>
-      <Button className="w-full cursor-pointer" variant="link" asChild>
-        <Link href="/sign-up">Não tem uma conta? Crie uma.</Link>
-      </Button>
-
-      <Separator />
-      <Button
-        type="submit"
-        variant="outline"
-        className="w-full flex items-center"
-      >
-        <Image
-          src={githubIcon}
-          alt="Github icon"
-          className="size-4 mr-2 dark:invert"
-        />
-        Entrar com o Github
-      </Button>
-    </form>
-  )
+  return <SignInForm />
 }
 
 export default SignIn
