5e2b21eb726ff6f88d0387af8b6f30dec44ff50a
Gui-dev <adrianon10@gmail.com>
2025-11-20 04:33:07 -0300

    feat: add get user profile

--- apps/web/src/auth/auth.ts
+++ apps/web/src/auth/auth.ts
@@ -1,8 +1,26 @@
+import { getProfile } from '@/http/get-profile'
 import { cookies } from 'next/headers'
+import { redirect } from 'next/navigation'
 
 export const isAuthenticated = async () => {
   const cookieStore = await cookies()
   const token = cookieStore.get('saas-token')
 
   return !!token
+}
+
+export const auth = async () => {
+  const cookieStore = await cookies()
+  const token = cookieStore.get('saas-token')
+
+  if (!token) {
+    redirect('/sign-in')
+  }
+
+  try {
+    const { user } = await getProfile()
+    return { user }
+  } catch (error) {
+    redirect('/api/auth/sign-out')
+  }
 }
