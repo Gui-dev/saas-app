1eaecec911793212957e5eedd7e7b3ea2c08c148
Gui-dev <adrianon10@gmail.com>
2025-11-24 19:40:00 -0300

    feat: add middleware to get organization

---
+++ apps/web/src/app/(protected)/org/[slug]/page.tsx
@@ -0,0 +1,12 @@
+import { Header } from '@/components/header'
+
+const OrganizationDetails = () => {
+  return (
+    <div className="flex flex-col gap-4">
+      <Header />
+      <h1>DETAILS</h1>
+    </div>
+  )
+}
+
+export default OrganizationDetails
