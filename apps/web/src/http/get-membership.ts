db83657404b3b577cb394ccfea46ba1ea82c6c11
Gui-dev <adrianon10@gmail.com>
2025-11-25 04:43:04 -0300

    feat: add get membership http request

---
+++ apps/web/src/http/get-membership.ts
@@ -0,0 +1,21 @@
+import { Role } from '@saas/auth'
+import { api } from './api-client'
+
+export interface IGetMembershipResponse {
+  membership: {
+    id: string
+    organizationId: string
+    userId: string
+    role: Role
+  }
+}
+
+export const getMembership = async (
+  slug: string
+): Promise<IGetMembershipResponse> => {
+  const membership = await api
+    .get(`organizations/${slug}/membership`)
+    .json<IGetMembershipResponse>()
+
+  return membership
+}
