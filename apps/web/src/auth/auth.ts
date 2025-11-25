7c0894172460e2d1fc2ca614e8e7c357c6c24aac
Gui-dev <adrianon10@gmail.com>
2025-11-25 04:44:24 -0300

    feat: add helper functions getCurrentOrganization, getCurrentMembership and ability

--- apps/web/src/auth/auth.ts
+++ apps/web/src/auth/auth.ts
@@ -1,12 +1,45 @@
+import { getMembership } from '@/http/get-membership'
 import { getProfile } from '@/http/get-profile'
+import { defineAbilityFor } from '@saas/auth'
 import { cookies } from 'next/headers'
 import { redirect } from 'next/navigation'
 
 export const isAuthenticated = async () => {
   const cookieStore = await cookies()
   const token = cookieStore.get('saas-token')
-
   return !!token
+}
+
+export const getCurrentOrganization = async () => {
+  const cookieStore = await cookies()
+  return cookieStore.get('org')?.value ?? null
+}
+
+export const getCurrentMembership = async () => {
+  const org = await getCurrentOrganization()
+
+  if (!org) {
+    return null
+  }
+
+  const { membership } = await getMembership(org)
+
+  return membership
+}
+
+export const ability = async () => {
+  const membership = await getCurrentMembership()
+
+  if (!membership) {
+    return null
+  }
+
+  const ability = defineAbilityFor({
+    id: membership.userId,
+    role: membership.role,
+  })
+
+  return ability
 }
 
 export const auth = async () => {
