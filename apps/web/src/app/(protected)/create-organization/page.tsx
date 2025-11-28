df1d7e411c7a889a381820774c82f456a4316652
Gui-dev <adrianon10@gmail.com>
2025-11-28 04:37:12 -0300

    feat: add create organization page

---
+++ apps/web/src/app/(protected)/create-organization/page.tsx
@@ -0,0 +1,17 @@
+import { Header } from '@/components/header'
+import { OrganizationForm } from './organization-form'
+
+const CreateOrganization = () => {
+  return (
+    <div className="py-4 space-y-4">
+      <Header />
+      <main className="mx-auto w-full max-w-[1200px] space-y-4">
+        <h1 className="text-2xl font-bold">Criar uma nova organizacao</h1>
+
+        <OrganizationForm />
+      </main>
+    </div>
+  )
+}
+
+export default CreateOrganization
