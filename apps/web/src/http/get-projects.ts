690152262ea376ded1ce70399f9cab5d88091e9f
Gui-dev <adrianon10@gmail.com>
2025-12-01 20:27:17 -0300

    feat: add get projects http request

---
+++ apps/web/src/http/get-projects.ts
@@ -0,0 +1,29 @@
+import { api } from './api-client'
+
+export interface IGetProjectsResponse {
+  projects: {
+    id: string
+    ownerId: string
+    organizationId: string
+    name: string
+    description: string
+    slug: string
+    avatarUrl: string | null
+    createdAt: Date
+    owner: {
+      select: {
+        id: string
+        name: string | null
+        avatarUrl: string | null
+      }
+    }
+  }[]
+}
+
+export const getProjects = async (slug: string) => {
+  const projects = await api
+    .get(`organizations/${slug}/projects`)
+    .json<IGetProjectsResponse>()
+
+  return projects
+}
