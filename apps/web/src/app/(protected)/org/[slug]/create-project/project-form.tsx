2e877006a0ac6188d28d7b26b62db7678c21d71c
Gui-dev <adrianon10@gmail.com>
2025-12-03 04:52:39 -0300

    feat: add project form component

---
+++ apps/web/src/app/(protected)/org/[slug]/create-project/project-form.tsx
@@ -0,0 +1,74 @@
+'use client'
+
+import { AlertTriangle, Loader2 } from 'lucide-react'
+
+import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert'
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { useFormState } from '@/hooks/use-form-state'
+import { createProjectAction } from './actions'
+import { Textarea } from '@/components/ui/textarea'
+
+export const ProjectForm = () => {
+  const [{ success, message, errors }, handleSubmit, isPending] =
+    useFormState(createProjectAction)
+
+  return (
+    <form onSubmit={handleSubmit} className="space-y-4">
+      {!success && message && (
+        <Alert variant="destructive">
+          <AlertTriangle className="size-4" />
+          <AlertTitle>Erro ao criar projeto</AlertTitle>
+          <AlertDescription>
+            <p>{message}</p>
+          </AlertDescription>
+        </Alert>
+      )}
+
+      {success && message && (
+        <Alert variant="success">
+          <AlertTriangle className="size-4" />
+          <AlertTitle>Tudo certo</AlertTitle>
+          <AlertDescription>
+            <p>{message}</p>
+          </AlertDescription>
+        </Alert>
+      )}
+
+      <div className="space-y-2">
+        <Label htmlFor="name">Nome do projeto</Label>
+        <Input
+          type="text"
+          name="name"
+          id="name"
+          placeholder="Digite o nome do seu projeto"
+        />
+        {errors?.name && (
+          <p className="text-xs font-medium text-red-500 dark:text-red-400">
+            {errors.name[0]}
+          </p>
+        )}
+      </div>
+
+      <div className="space-y-2">
+        <Label htmlFor="domain">Descrição</Label>
+        <Textarea
+          name="description"
+          id="description"
+          placeholder="Digite uma descrição do seu projeto"
+        />
+        {errors?.description && (
+          <p className="text-xs font-medium text-red-500 dark:text-red-400">
+            {errors.description[0]}
+          </p>
+        )}
+      </div>
+
+      <Button type="submit" className="w-full" disabled={isPending}>
+        {isPending && <Loader2 className="h-4 w-4 animate-spin" />}
+        {!isPending && 'Salvar projeto'}
+      </Button>
+    </form>
+  )
+}
