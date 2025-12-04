f72efb4a5ce784300d02f20dca6a5e61c418ecae
Gui-dev <adrianon10@gmail.com>
2025-12-04 05:25:53 -0300

    feat: add nav links component

---
+++ apps/web/src/components/nav-links.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import { ComponentProps } from 'react'
+
+interface NavLinkProps extends ComponentProps<typeof Link> {}
+
+export const NavLink = (props: NavLinkProps) => {
+  const pathName = usePathname()
+  const isCurrent = props.href.toString() === pathName
+
+  return <Link data-current={isCurrent} {...props} />
+}
