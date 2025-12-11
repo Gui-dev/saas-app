ed9361b4ddf9443f697a9b3d7378121f87ab2940
Gui-dev <adrianon10@gmail.com>
2025-12-11 05:21:08 -0300

    feat: add shutdown organization button component

---
+++ apps/web/src/app/(protected)/org/[slug]/settings/shutdown-organization-button.tsx
@@ -0,0 +1,28 @@
+import { getCurrentOrganization } from '@/auth/auth'
+import { Button } from '@/components/ui/button'
+import { shutdownOrganization } from '@/http/shutdown-organization'
+import { XCircle } from 'lucide-react'
+import { redirect } from 'next/navigation'
+
+export const ShutdownOrganizationButton = () => {
+  const shutdownOrganizationAction = async () => {
+    'use server'
+
+    const currentOrg = await getCurrentOrganization()
+    await shutdownOrganization({ org: currentOrg! })
+
+    redirect('/')
+  }
+  return (
+    <form action={shutdownOrganizationAction}>
+      <Button
+        type="submit"
+        variant="destructive"
+        className="w-56 cursor-pointer"
+      >
+        <XCircle className="mr-2 size-4" />
+        Deletar organização
+      </Button>
+    </form>
+  )
+}
