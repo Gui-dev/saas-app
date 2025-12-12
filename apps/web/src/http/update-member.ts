3dc4e594fcd84186fe1d42a1ebd17ca930a53ea0
Gui-dev <adrianon10@gmail.com>
2025-12-12 05:04:02 -0300

    feat: add update member http request

---
+++ apps/web/src/http/update-member.ts
@@ -0,0 +1,20 @@
+import { Role } from '@saas/auth'
+import { api } from './api-client'
+
+export interface IUpdateMemberRequest {
+  org: string
+  memberId: string
+  role: Role
+}
+
+export const updateMember = async ({
+  org,
+  memberId,
+  role,
+}: IUpdateMemberRequest): Promise<void> => {
+  await api.put(`organizations/${org}/members/${memberId}`, {
+    json: {
+      role,
+    },
+  })
+}
