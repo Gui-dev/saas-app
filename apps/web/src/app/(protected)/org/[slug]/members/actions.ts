75e925d03914f2fc651e646264e89289ef546afe
Gui-dev <adrianon10@gmail.com>
2025-12-12 05:04:43 -0300

    feat: add helper function updateMemberAction

--- apps/web/src/app/(protected)/org/[slug]/members/actions.ts
+++ apps/web/src/app/(protected)/org/[slug]/members/actions.ts
@@ -1,5 +1,9 @@
+'use server'
+
 import { getCurrentOrganization } from '@/auth/auth'
 import { removeMember } from '@/http/remove-member'
+import { updateMember } from '@/http/update-member'
+import { Role } from '@saas/auth'
 import { revalidateTag } from 'next/cache'
 
 interface IRemoveMemberActionRequest {
@@ -11,6 +15,21 @@ export const removeMemberAction = async ({
 }: IRemoveMemberActionRequest) => {
   const currentOrg = await getCurrentOrganization()
   await removeMember({ org: currentOrg!, memberId })
+
+  revalidateTag(`${currentOrg}/members`, 'max')
+}
+
+interface IUpdateMemberActionRequest {
+  memberId: string
+  role: Role
+}
+
+export const updateMemberAction = async ({
+  memberId,
+  role,
+}: IUpdateMemberActionRequest) => {
+  const currentOrg = await getCurrentOrganization()
+  await updateMember({ org: currentOrg!, memberId, role })
 
   revalidateTag(`${currentOrg}/members`, 'max')
 }
